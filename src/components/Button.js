23393eedec82757fd47481266f849db642a761e3
Nasouh Al turk <nasouh@live.com>
2019-10-28 12:34:03 -0400

    set up the project, and did the button assignment

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,23 @@
 import React from "react";
 
 import "components/Button.scss";
+import { action } from "@storybook/addon-actions/dist/preview";
+
+var classnames = require('classnames');
 
 export default function Button(props) {
-   return <></>;
-}
+  const buttonClass = classnames("button", {
+     "button--confirm": props.confirm,
+     "button--danger": props.danger
+  });
+
+   return (
+      <button
+        className={buttonClass}
+        onClick={props.onClick}
+        disabled={props.disabled}
+      >
+        {props.children}
+      </button>
+    );
+  }
