8a276011fb23959f3a1f35fffefa277a6527f5c0
Nasouh Al turk <nasouh@live.com>
2019-10-29 16:59:09 -0400

    added axios to the application and now getting the days

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,27 +1,10 @@
-import React, { useState } from "react";
-
+import React, { useState , useEffect} from "react";
+import axios from "axios";
 
 import "components/Application.scss";
 import DayList from "components/DayList"
 import Appointment from "components/Appointment"
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
 
 const appointments = [
   {
@@ -73,7 +56,15 @@ const appointments = [
 export default function Application(props) {
 
   const [ day, setDay ] = useState("Monday")
-  
+  const [ days, setDays ] = useState([])
+
+  useEffect(() => {
+    axios.get("/api/days")
+    .then(response => {
+      setDays(response.data)
+    })
+  }, [])
+
   return (
     <main className="layout">
       <section className="sidebar">
