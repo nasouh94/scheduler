8ff5f59fe3462c43c4bbb8c8de678c09b333813e
Nasouh Al turk <nasouh@live.com>
2019-10-29 18:29:57 -0400

    added a helper function that gets appointment by day

---
+++ src/helpers/selectors.js
@@ -0,0 +1,13 @@
+export const getAppointmentsForDay = (state, day) => {
+  const appointmentsId = state.days
+    .filter(e => e.name === day)
+    .map(e => e.appointments)
+    .reduce((acc, val) => acc.concat(val), []);
+
+  const appointment = [];
+  appointmentsId.forEach(e => {
+    appointment.push(state.appointments[e]);
+  });
+
+  return appointment;
+}
