1ae60b66ba11c366243a8d2267f1064b1e126dea
Nasouh Al turk <nasouh@live.com>
2019-11-04 18:40:45 -0500

    did further testing on application.tests

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -44,7 +44,8 @@ export default function Appointment(props) {
 	}
 
 	return (
-		<article className="appointment">
+		<article className="appointment" data-testid="appointment">
+			
 			<Header time={props.time} />
 
 			{mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
